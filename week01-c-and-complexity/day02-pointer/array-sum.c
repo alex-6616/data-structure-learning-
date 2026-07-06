490ae9f3a344ccb35d6daed5835d46fbf71b06ae
alex-6616 <811827647@qq.com>
2026-07-06 21:33:51 +0800

    Update array-sum.c

--- week01-c-and-complexity/day02-pointer/array-sum.c
+++ week01-c-and-complexity/day02-pointer/array-sum.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int sum_array(int *,int);
+
+int main(){
+	int n;
+	int s[20];
+	scanf("%d",&n);
+	for(int i = 0; i < n; i++){
+		scanf("%d",&s[i]);
+	}
+	printf("%d",sum_array(s,n));
+	return 0;
+} 
+
+int sum_array(int *a,int n){
+	int sum = 0;
+	for(int *p = a;p < a+n; p++){
+		sum += *p;
+	}
+	return sum;
+}
